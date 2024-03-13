8526b102bfccd7beda245d5dafafc48a943722b1
jeiel <jeielgomes.s6@gmail.com>
2024-03-13 10:59:57 -0300

    first commit

---
+++ verf_num_maior.py
@@ -0,0 +1,9 @@
+#Cod. para verificar se um numero e maior que o outro
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite outro numero: '))
+if n1 > n2:
+    print (n1, 'e maior que', n2)
+elif n1 < n2:
+    print (n1, 'e menor que', n2)
+else:
+    print (n1, 'e iqual a', n2)
